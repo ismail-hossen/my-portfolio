51dcbf42327588b9555edd3f9a11e11896ffab8c
ismail-hossen <satict2016@gmail.com>
2022-06-09 20:51:41 +0600

    added formspree for get massage from form field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Home from "./components/Home";
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,47 @@
 import React from "react";
 import "./Contact.css";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xzbowgbn");
   return (
     <section className="container">
       <h1>
-        Thanks for taking the time to reach out. How <br /> can I help you today?
+        Thanks for taking the time to reach out. How <br /> can I help you
+        today?
       </h1>
 
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="input-field-area">
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
+            <input type="text" id="name" name="name" />
+            <ValidationError prefix="Name" field="name" errors={state.errors} />
           </div>
 
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input id="email" type="email" name="email" />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
           </div>
         </div>
-
         <div className="form-group">
           <label htmlFor="massage">Massage</label>
-          <textarea id="massage" cols="30" rows="10" />
+          <textarea id="massage" name="massage" cols="30" rows="10" />
+          <ValidationError
+            prefix="Massage"
+            field="massage"
+            errors={state.errors}
+          />
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+        {state.succeeded && <p style={{ color: "red" }}>Thanks for joining!</p>}
       </form>
     </section>
   );
